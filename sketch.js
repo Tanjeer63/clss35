1606b9031b2bd59be2b57e9b53ae1095390563a8
Tanjeer63 <75249275+Tanjeer63@users.noreply.github.com>
2021-02-26 13:29:46 +0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var hypnoticBall
+var database;
+var position;
+
+function setup(){
+    database = firebase.database();
+    console.log(database);
+
+    
+
+    createCanvas(500,500);
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+
+    var hypnoticBallPosition = database.ref('ball/position');
+    hypnoticBallPosition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
